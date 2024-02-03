0204f24cb0d2737225d88fe48f1532aa7d7e4eac
rakeshjain1111 <rakeshkjain222@gmail.com>
2024-02-03 17:41:02 +0530

    some update

--- src/components/HttpRequest.jsx
+++ src/components/HttpRequest.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import 'font-awesome/css/font-awesome.css';
+import 'font-awesome/css/font-awesome.min.css';
 import $ from "jquery";
 
 function HttpRequest() {
     const [allData, setAllData] = useState([]);
     const [oneData, setOneData] = useState([]);
-    const [counter, setCounter]=useState(1);
+    let [counter, setCounter]=useState(1);
+    const [btnState, setBtnState]=useState(true);
 
     function ajaxRequest(){
         
@@ -33,12 +34,24 @@ function HttpRequest() {
             }
         })
     }
+
+    useEffect(()=>{
+        ajaxRequest();
+    },[])
    
     useEffect(()=>{
         // ajaxRequest();
         httpRequest();
     
     },[counter])
+
+
+    function setCount(){
+        if(counter>0){
+            counter=counter-1;
+            setCounter(counter);
+        }
+    }
 
     function Card({item}){
         return(
@@ -59,23 +72,33 @@ function HttpRequest() {
      <div className='Container rounded mt-2 bg-opacity-10 py-1 px-5 bg-primary'>
         <h1 className='text-center'>HttpRequest</h1>
         <div className='d-flex justify-content-between'>
+            <Button className='bg-primary' onClick={()=>setBtnState(true)}>Show All Comment</Button>
+            <Button className='bg-primary' onClick={()=>setBtnState(false)}>Show Single Comment</Button>
+        </div>
+        <div className='d-flex justify-content-between'>
             <div className='display-5 fw-bold'>Comment</div>
             <div className='fs-4 shadow-sm bg-secondary bg-opacity-25 rounded px-2'>New Comment</div>
         </div>
+
         {
+            btnState?
+            allData.map((item,index)=>{
+                return <Card item={item} key={index}/>
+            })
+            :
             oneData.map((item,index)=>{
                 return <Card item={item} key={index}/>
             })
         }
-        <div className='float-end'>
-            <Button className='me-2' onClick={()=>{setCounter(counter-1)}}>
-                <i className='fa fa-angle-left'></i>
-            </Button>
-            <Button className='me-2' onClick={()=>{setCounter(counter+1)}}>
-                <i className='fa fa-angle-right'></i>
-            </Button>
 
-            <h1>{counter}</h1>
+        {/* {
+            oneData.map((item,index)=>{
+                return <Card item={item} key={index}/>
+            })
+        } */}
+        <div className='d-flex justify-content-between'>
+            <Button className='me-2' onClick={setCount}>Prev </Button>
+            <Button className='me-2' onClick={()=>{setCounter(counter+1)}}>Next </Button>
         </div>
      </div>
    </>
